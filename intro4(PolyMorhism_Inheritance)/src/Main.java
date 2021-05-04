5c38e5edfbf07efa22b0b125fc5de644f4d84d92
simse <simse@DESKTOP-LR3BPNU>
2021-05-04 17:54:55 +0300

    Polymorhism Deneme(Inheritance ile!)

--- intro4(PolyMorhism_Inheritance)/src/Main.java
+++ intro4(PolyMorhism_Inheritance)/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		CustomerManager customerManager=new CustomerManager();
-		customerManager.add(new EmailLogger()); //bur
+		customerManager.add(new EmailLogger()); //Burada ki mantýðýmýz add içerisine hangi Log'u new'lersek o çalýþacaktýr!!
 
 	}
 
